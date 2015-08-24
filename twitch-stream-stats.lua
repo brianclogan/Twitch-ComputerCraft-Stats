f4e07b72b85656eed48c0395d441d13d53e591fd
Brian Logan <logan@collingmedia.com>
2015-08-24 15:23:15 -0700

    Update twitch-stream-stats.lua

--- twitch-stream-stats.lua
+++ twitch-stream-stats.lua
@@ -1,5 +1,5 @@
 ----- EDITABLE VARIABLES. Only edit the below.
-streamid = "darkgoldblade01" --- the beam.pro username
+streamid = "darkgoldblade01" --- the twitch.tv username
 SleepTime = 60 --- how often do you want to refresh in seconds? (default - 60)
 direction = "right" --- this is the direction the monitors are
 --- This next area is for deciding what you want on each line. The options are: followerCount, viewerCount, and currentGame
